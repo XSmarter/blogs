a6981d44f12eb9ba7ef533fc4c60129fe4ac3859
XSmarter <XSmarter@outlook.com>
2018-10-10 12:08:42 +0800

    commit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   title: '风散云烟',
   description: '风散云烟博客',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
 }

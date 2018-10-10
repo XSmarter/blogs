13bdb80819fb5571283bacf4e6e39c85683b4dd5
XSmarter <XSmarter@outlook.com>
2018-10-10 17:41:47 +0800

    commit

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,10 +7,10 @@ module.exports = {
     ],
     sidebar: [
       ['/','主页'],
-      ['/technical/Express和jwt认证实现RESTful-API','Express和jwt认证实现RESTful-API'],
-      ['/technical/Vue-table-component','Vue-table-component'],
-      ['/technical/一些常用的javascript代码','一些常用的javascript代码'],
-      ['/technical/使用SheetJS实现纯前端导出Excel数据','使用SheetJS实现纯前端导出Excel数据']
+      ['/technical/Express和jwt认证实现RESTful-API.html','Express和jwt认证实现RESTful-API'],
+      ['/technical/Vue-table-component.html','Vue-table-component'],
+      ['/technical/一些常用的javascript代码.html','一些常用的javascript代码'],
+      ['/technical/使用SheetJS实现纯前端导出Excel数据.html','使用SheetJS实现纯前端导出Excel数据']
     ]
   },
   markdown: {
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 title: 首页
 date: 2018-10-10 00:00:00
 prev: false
-next: /technical/Express和jwt认证实现RESTful-API.md
+next: /technical/Express和jwt认证实现RESTful-API.html
 ---
 博客

734d68cf8c9e3f1af10fa19e27e9ae0f6f8c8cf9
XSmarter <XSmarter@outlook.com>
2019-04-26 12:02:27 +0800

    1

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
     title: 'Hello VuePress',
-    description: 'Just playing around'
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Technical', link: '/technical/' }
+        ]
+    }
 }

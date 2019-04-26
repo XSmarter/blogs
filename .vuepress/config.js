3b626b9876b46cde31a622284695157c33406b99
XSmarter <XSmarter@outlook.com>
2019-04-26 12:57:35 +0800

    1

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,8 +3,15 @@ module.exports = {
     description: '云烟终被风散',
     themeConfig: {
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Technical', link: '/technical/' }
+            { text: '主页', link: '/' },
+            { 
+                text: '前端技术', 
+                items: [
+                    { text: 'Vue', link: '/technical/vue/' },
+                    { text: 'Electron', link: '/technical/electron/' },
+                    { text: '其他杂记', link: '/technical/other/' }
+                ]
+            }
         ]
     }
 }
